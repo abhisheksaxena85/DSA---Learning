e2a578987f2707d503c39fb38cf26e1573201e11
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2024-11-26 22:34:59 +0530

    Pattern printing in JAVA [Piramid]

--- Chapter1/chapter_1.java
+++ Chapter1/chapter_1.java
@@ -22,7 +22,14 @@ public class chapter_1 {
         //         System.out.println(s1+"        "+x);
         //     }
         //     System.out.println("================================");
-        for(int i=4; i>=0; i--){
+        // for(int i=4; i>=0; i--){
+        //     for(int j= 1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        for(int i=0; i<=4; i++){
             for(int j= 1; j<=i; j++){
                 System.out.print("*");
             }
