fe4de5793ef13fe734c47eb5636003711a5365eb
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2024-12-05 00:44:03 +0530

    Starting Array Data Structure

---
+++ Chapter2/chapter_2.java
@@ -0,0 +1,10 @@
+/* 
+        Starting Array Data Structure
+        start date - 5 Dec, 12:43 AM
+ */
+
+public class chapter_2{
+    public static void main(String[] value){
+        System.err.println("Hello World");
+    }
+}
