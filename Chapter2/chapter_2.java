a794040a15871d113b35c8155ee736af1e1dbb0d
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2024-12-30 20:18:28 +0530

    Remaining changes of array chapter from DSA

--- Chapter2/chapter_2.java
+++ Chapter2/chapter_2.java
@@ -1,3 +1,5 @@
+
+
 /* 
         Starting Array Data Structure
         start date - 5 Dec, 12:43 AM
@@ -5,6 +7,27 @@
 
 public class chapter_2{
     public static void main(String[] value){
-        System.err.println("Hello World");
+        // int marks[] = {1,2,3,4,5,6};
+        // int marks2[] = {1,2,3,4,5,6};
+        // System.out.println(marks.equals(marks2[0]));
+
+        // int marks2[] = {1,2,3,4,5,6};
+
+        // int marks[] = {1,4,3};
+        // marks[0] = 5;
+        // marks[1] = 10;
+        // marks[2] = 15;
+        // System.out.println(Arrays.toString(marks));
+
+        // ArrayList<Float> markValues = new ArrayList<>();
+        // markValues.add(1.26f);
+        // markValues.add(2.31f);
+        // String val = markValues.toString();
+        // System.out.println(markValues);
+
+        int limit = 120;
+
+        // int val[] = new int[limit];
+        
     }
 }
