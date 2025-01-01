30c698bcad1e3a6dec6c02ff505b4ad54ffc3ded
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-01-01 23:41:06 +0530

    Binary Search Program

--- Chapter2/chapter_2.java
+++ Chapter2/chapter_2.java
@@ -32,22 +32,49 @@ public class chapter_2{
         // int numbers[] = {12, 123,2, 6, 98, 76, 56, 74, 78, 23, 52, 16, 9,4};
         // int searchedIndex = linearSearch(numbers, 2);
         // System.out.println(searchedIndex);
+        
 
-        int numbers[] = {12, 123, 6, 98, 76, 56, 74, 78, 23, 52, 16, 9,4};
-        int smallestNumber = smallestNumber(numbers);
-        System.out.println("Smallest number : "+smallestNumber);
+        // int numbers[] = {12, 123, 6, 98, 76, 56, 74, 78, 23, 52, 16, 9,4};
+        // int smallestNumber = smallestNumber(numbers);
+        // System.out.println("Smallest number : "+smallestNumber);
+
+
+        /// Binary Search
+        /// There must be a sorted array for binary search
+        // Sorted Array 
+        int sortedArr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12, 13};
+        // int foundValue = binarySearch(sortedArr, 90);
+        System.out.println("Found value :"+sortedArr.length);
     }
 
-    public static int smallestNumber(int numbers[]){
-        int smallest = Integer.MAX_VALUE;
+    public static int binarySearch(int sortedArr[], int key){
+        int start = 0;
+        int end = sortedArr.length-1;
 
-        for(int i = 0; i<numbers.length; i++){
-            if(smallest > numbers[i]){
-                smallest = numbers[i];
+        while(start<=end){
+            int mid = (start+end)/2;
+
+            if(sortedArr[mid] == key){
+                return mid;
+            }else if(sortedArr[mid] > key){
+                end = mid-1;
+            }else{
+                start = mid+1;
             }
         }
-        return smallest;
+        return -1;
     }
+
+    // public static int smallestNumber(int numbers[]){
+    //     int smallest = Integer.MAX_VALUE;
+
+    //     for(int i = 0; i<numbers.length; i++){
+    //         if(smallest > numbers[i]){
+    //             smallest = numbers[i];
+    //         }
+    //     }
+    //     return smallest;
+    // }
 
     // public static int linearSearch(int numbers[], int key){
     //     for(int i = 0; i<numbers.length;i++){
