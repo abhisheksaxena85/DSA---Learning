20e554be9b236f2fa5966abef5c6e5784e0b63cf
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-01-18 19:21:23 +0530

    String Operations like Char Arrays

--- Chapter5/chapter_5.java
+++ Chapter5/chapter_5.java
@@ -1,13 +1,24 @@
 /*
-
     Starting Strings In Java
     Start Date - January 17th, 2025 12:34 AM
-
  */
-
-
 public class chapter_5{
     public static void main(String[] arg){
-        System.out.println("Hello world");
+        // String name = "Abhishek Saxena";
+        // System.out.println(name);
+        // Scanner sc = new Scanner(System.in);
+        // String name;
+        // name = sc.nextLine();
+        // System.err.println(name.contains("a"));
+
+        String name = "Mashroom-on-the-ground";
+        operations(name);
+    }
+
+    /// Basic Operations on String 
+    public static void operations(String stringValue){
+        for(int i = 0;i<stringValue.length();i++){
+            System.out.print(stringValue.charAt(i)+" ");
+        }
     }
 }
