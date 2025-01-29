1cf5f8be9ccc88982f20779c84ad45d4ba9d1932
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-01-29 23:52:18 +0530

    LeetCode Question no.7: Reverse the number entered by user

--- leetcode_solution/leetcode_solution.java
+++ leetcode_solution/leetcode_solution.java
@@ -2,31 +2,56 @@
 public class leetcode_solution {
 
     public static void main(String[] args) {
-        System.out.println("LeetCode: ");
-        int value = 1000;
-        System.out.println(isPalindrome(value));
+        // System.out.println("LeetCode: ");
+        // int value = 1000;
+        // System.out.println(isPalindrome(value));
+        int value = 1534236469;
+        System.out.println(reverseNumber(value));
     }
 
-    public static boolean isPalindrome(Integer x) {
+    /// Reverse the input int value only in 32bit signed integer, If it crosses 32bit limit then return 0;
+    public static int reverseNumber(int x) {
         int a = x;
-        int r = 0;
-        while (x > 0) {
-            int l = x % 10;
-            x = x / 10;
-            r = (r * 10) + l;
+        long r = 0;
+        if(x>0){
+            while (a > 0) {
+                int l = a % 10;
+                a = a / 10;
+                r = (r * 10) + l;
+            }
+        }else{
+            while (a < 0) {
+                int l = a % 10;
+                a = a / 10;
+                r = (r * 10) + l;
+            }
         }
-        if (a == r) {
-            return true;
+        if (r < 2147483648L && r > -2147483649L) {
+            return (int) r;
+        } else {
+            return 0;
         }
-        return false;
-
-        // for(int i = 0; i<value.toString().length();i++){
-        //     if(value.toString().charAt(i) != value.toString().charAt(value.toString().length()-1-i)){
-        //         System.out.println("It is not palindrom");
-        //         return false;
-        //     }
-        // }
-        // System.out.println("It is palindrome");
-        // return true;
     }
+
+    // public static boolean isPalindrome(Integer x) {
+    //     int a = x;
+    //     int r = 0;
+    //     while (x > 0) {
+    //         int l = x % 10;
+    //         x = x / 10;
+    //         r = (r * 10) + l;
+    //     }
+    //     if (a == r) {
+    //         return true;
+    //     }
+    //     return false;
+    // for(int i = 0; i<value.toString().length();i++){
+    //     if(value.toString().charAt(i) != value.toString().charAt(value.toString().length()-1-i)){
+    //         System.out.println("It is not palindrom");
+    //         return false;
+    //     }
+    // }
+    // System.out.println("It is palindrome");
+    // return true;
+    // }
 }
