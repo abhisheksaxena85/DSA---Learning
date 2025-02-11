cbb37140d481a9865064b5bceeb8f3d1ad7cf59d
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-02-11 22:26:19 +0530

    Get the bit of ith place of binary digits

--- Chapter6/chapter_6.java
+++ Chapter6/chapter_6.java
@@ -3,16 +3,25 @@
     Start Date - January 29th, 2025 10:27 PM
  */
 
-public class chapter_6{
-    public static void main(String[] args){
-        int a = 5; 
-        int b = 2147483647; 
-        int c = a&b;   
-        System.err.println("Is value Even: "+isEven(11));
+public class chapter_6 {
+
+    public static void main(String[] args) {
+        // int a = 5; 
+        // int b = 2147483647; 
+        // int c = a&b;   
+        // System.err.println("Is value Even: "+isEven(11));
+        System.out.println("Bit at ith place " + getBit(5, 4));
     }
 
-    /// Finding out if value is Odd or even by calculating on the bit level
-    public static boolean isEven(int value){
-        return (value&1) != 1;
+    /// Gettting the bit of the Ith place 
+    public static int getBit(int n, int i) {
+        int x = n & (1 << i);
+        return x == 0 ? 0 : 1;
     }
+
+
+/// Finding out if value is Odd or even by calculating on the bit level
+    // public static boolean isEven(int x){
+    //     return (x&1)!=1;
+    // }
 }
