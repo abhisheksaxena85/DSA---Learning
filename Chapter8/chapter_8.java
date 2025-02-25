ad7533f9a9e2ba73f8b12c7024bee3eee52d7bd3
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-02-25 10:45:52 +0530

    Printing values from 1 to 10 using the recursive function

--- Chapter8/chapter_8.java
+++ Chapter8/chapter_8.java
@@ -7,5 +7,17 @@ public class chapter_8 {
 
     public static void main(String args[]) {
         System.out.println("Hello World!");
+        printValue(10);
+    }
+
+    /// Printing values from 1 to 10 using the recursive functions
+    public static int printValue(int n) {
+        // Base Case
+        if (n == 1) {
+            System.out.println("1");
+            return 1;
+        }
+        System.out.println(n);
+        return printValue(n - 1);
     }
 }
