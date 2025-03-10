aadf63bcf1c4b552469973781145fd8e39e5ffc2
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-03-10 12:53:24 +0530

    Modifying An array on backtracking in recursion

--- Chapter11/chapter_11.java
+++ Chapter11/chapter_11.java
@@ -6,6 +6,26 @@
 public class chapter_11 {
 
     public static void main(String args[]) {
-        System.out.println("Starting Chapter Backtracking");
+        int arr[] = new int[5];
+
+        backtrackArr(arr, 0, 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+    public static void backtrackArr(int arr[], int index, int value) {
+        /// Base Case
+        if (index == arr.length) {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i]);
+            }
+            System.out.println();
+            return;
+        }
+
+        arr[index] = value;
+        backtrackArr(arr, index + 1, value + 1);
+        arr[index] = value - 2;
     }
 }
