6408b490e104338e7ea808d958c27122f8ecbd2d
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-03-11 11:18:28 +0530

    Printing all the possible subsets of an string using the backtracking in recursion

--- Chapter11/chapter_11.java
+++ Chapter11/chapter_11.java
@@ -8,24 +8,36 @@ public class chapter_11 {
     public static void main(String args[]) {
         int arr[] = new int[5];
 
-        backtrackArr(arr, 0, 1);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
-        }
+        // backtrackArr(arr, 0, 1);
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print(arr[i]);
+        // }
+        String str = "abc";
+        findSubset(str, 0, "");
     }
 
-    public static void backtrackArr(int arr[], int index, int value) {
-        /// Base Case
-        if (index == arr.length) {
-            for (int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i]);
-            }
-            System.out.println();
+    /// Printing all the subsets of an string
+    public static void findSubset(String str, int index, String value) {
+        if (index == str.length()) {
+            System.out.println(value);
             return;
         }
 
-        arr[index] = value;
-        backtrackArr(arr, index + 1, value + 1);
-        arr[index] = value - 2;
+        findSubset(str, index + 1, value + str.charAt(index));
+        findSubset(str, index + 1, value);
     }
+
+    // public static void backtrackArr(int arr[], int index, int value) {
+    //     /// Base Case
+    //     if (index == arr.length) {
+    //         for (int i = 0; i < arr.length; i++) {
+    //             System.out.print(arr[i]);
+    //         }
+    //         System.out.println();
+    //         return;
+    //     }
+    //     arr[index] = value;
+    //     backtrackArr(arr, index + 1, value + 1);
+    //     arr[index] = value - 2;
+    // }
 }
