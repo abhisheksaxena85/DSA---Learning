e59bafbb90ee2f2fba3e8e3f7f6cc35d2d71b78c
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-05-08 01:10:46 +0530

    Starting of Array List - Similar to Array but just a bit different

---
+++ Chapter12/chapter_12.java
@@ -0,0 +1,11 @@
+/*
+    Starting ArrayList
+    Start Date - March 04, 2025 Sunday 
+ */
+
+public class chapter_12 {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
