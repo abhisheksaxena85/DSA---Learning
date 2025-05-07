26e5c3313d0863938447f620090193d2da35637a
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-05-08 02:19:58 +0530

    Leetcode Question no.11 - 11. Container With Most Water

--- Chapter12/chapter_12.java
+++ Chapter12/chapter_12.java
@@ -1,11 +1,37 @@
 /*
     Starting ArrayList
-    Start Date - March 04, 2025 Sunday 
+    Start Date - May 04, 2025 Sunday 
  */
 
 public class chapter_12 {
 
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        int arr[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(arr));
     }
+
+    /// Container with most water - Leetcode Question Also
+    public static int maxArea(int[] h) {
+        int a = Integer.MIN_VALUE;
+        if (h.length == 0 || h.length == 1) {
+            return 0;
+        }
+        int s = 0;
+        int e = h.length - 1;
+        while (s <= e) {
+            int m = Math.min(h[s], h[e]);
+            int d = e - s;
+            int c = m * d;
+            if (c > a) {
+                a = c;
+            }
+            if (h[s] < h[e]) {
+                s++;
+            } else {
+                e--;
+            }
+        }
+        return a;
+    }
+
 }
