c47e5262b236ad09d1f95584370acd09816d71d7
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-05-14 01:32:13 +0530

    Starting of Linked List Chapter 13

---
+++ Chapter13/chapter_13.java
@@ -0,0 +1,11 @@
+/*
+    Starting LinkedList
+    Start Date - May 14, 2025, 1:00 AM
+ */
+
+public class chapter_13 {
+
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+    }
+}
