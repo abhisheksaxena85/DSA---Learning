ffe52b04d99383efc8ae8afd57d8167980b8f03c
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-05-14 17:50:56 +0530

    Structure of Node and LInkedList, along with add operations[AddFirst & AddLast, Print LinkedLIst]

--- Chapter13/chapter_13.java
+++ Chapter13/chapter_13.java
@@ -6,6 +6,64 @@
 public class chapter_13 {
 
     public static void main(String args[]) {
-        System.out.println("Hello world");
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.printLinkedList();
     }
+}
+
+class LinkedList {
+
+    public static Node head;
+    public static Node tail;
+
+    /// Basic Node Structure
+    public static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Addition in LinkedList is tow types - AddFirst, AddLast
+    // - AddFirst 
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // - AddLast
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    // Print Linked List
+    public void printLinkedList() {
+        if (head == null) {
+            System.out.println("Linked List is empty");
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
 }
