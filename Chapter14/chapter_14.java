f8d84fdd108e4d8d71a4a9ad89de4abe3f6a6226
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-06-08 01:37:39 +0530

    Starting the stack data-structure

---
+++ Chapter14/chapter_14.java
@@ -0,0 +1,11 @@
+/*
+    Starting Stack
+    Start Date - June 8, 2025, 1:24 AM
+ */
+
+public class chapter_14 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
