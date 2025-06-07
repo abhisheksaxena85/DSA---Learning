a9b1b562768a6633c1cc3683d5dbac5a832611c7
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-06-08 04:20:04 +0530

    Push data to bottom of Stack - Using recursion and backtracking

--- Chapter14/chapter_14.java
+++ Chapter14/chapter_14.java
@@ -1,4 +1,5 @@
 
+import java.util.Stack;
 
 /*
     Starting Stack
@@ -7,12 +8,14 @@
 public class chapter_14 {
 
     public static void main(String[] args) {
-        Stack s = new Stack();
+        Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
         s.push(4);
         s.push(5);
+
+        pushToBottom(s, 15);
 
         System.out.println("The stack is empty: " + s.isEmpty());
         while (!s.isEmpty()) {
@@ -21,66 +24,70 @@ public class chapter_14 {
         }
         System.out.println("The stack is empty: " + s.isEmpty());
     }
-}
 
-class Stack {
-
-    /// Stack using LinkedList
-    static Node head;
-
-    public boolean isEmpty() {
-        return head == null;
-    }
-
-    public void push(int data) {
-        Node newNode = new Node(data);
-        if (isEmpty()) {
-            head = newNode;
+    public static void pushToBottom(Stack<Integer> stack, int data) {
+        if (stack.isEmpty()) {
+            stack.push(data);
             return;
         }
-        newNode.next = head;
-        head = newNode;
-    }
 
-    public void pop() {
-        if (isEmpty()) {
-            System.out.println("Stack is empty");
-            return;
-        }
-        head = head.next;
-    }
-
-    public int peek() {
-        if (isEmpty()) {
-            System.out.println("Stack is empty");
-            return -1;
-        }
-        return head.data;
-    }
-
-    // /// Stack Using Array List
-    // ArrayList<Integer> list = new ArrayList<>();
-    // // Is Empty Function 
-    // public boolean isEmpty() {
-    //     return list.size() == 0;
-    // }
-    // public void push(int data) {
-    //     list.add(data);
-    // }
-    // public void pop() {
-    //     list.remove(list.size() - 1);
-    // }
-    // public int peek() {
-    //     return list.get(list.size() - 1);
-    // }
-    public class Node {
-
-        int data;
-        Node next;
-
-        Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
+        int topData = stack.peek();
+        stack.pop();
+        pushToBottom(stack, data);
+        stack.push(topData);
     }
 }
+
+// class Stack {
+//     /// Stack using LinkedList
+//     static Node head;
+//     public boolean isEmpty() {
+//         return head == null;
+//     }
+//     public void push(int data) {
+//         Node newNode = new Node(data);
+//         if (isEmpty()) {
+//             head = newNode;
+//             return;
+//         }
+//         newNode.next = head;
+//         head = newNode;
+//     }
+//     public void pop() {
+//         if (isEmpty()) {
+//             System.out.println("Stack is empty");
+//             return;
+//         }
+//         head = head.next;
+//     }
+//     public int peek() {
+//         if (isEmpty()) {
+//             System.out.println("Stack is empty");
+//             return -1;
+//         }
+//         return head.data;
+//     }
+//     /// Stack Using Array List
+//     ArrayList<Integer> list = new ArrayList<>();
+//     // Is Empty Function 
+//     public boolean isEmpty() {
+//         return list.size() == 0;
+//     }
+//     public void push(int data) {
+//         list.add(data);
+//     }
+//     public void pop() {
+//         list.remove(list.size() - 1);
+//     }
+//     public int peek() {
+//         return list.get(list.size() - 1);
+//     }
+//     public class Node {
+//         int data;
+//         Node next;
+//         Node(int data) {
+//             this.data = data;
+//             this.next = null;
+//         }
+//     }
+// }
