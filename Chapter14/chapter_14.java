054c189220a5f98cefbe4a6793d4bcf35632e362
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-06-08 03:13:10 +0530

    Custom Stack Data-Structure using LInkedList

--- Chapter14/chapter_14.java
+++ Chapter14/chapter_14.java
@@ -1,5 +1,4 @@
 
-import java.util.ArrayList;
 
 /*
     Starting Stack
@@ -26,24 +25,62 @@ public class chapter_14 {
 
 class Stack {
 
-    /// Stack Using Array List
-    ArrayList<Integer> list = new ArrayList<>();
+    /// Stack using LinkedList
+    static Node head;
 
-    // Is Empty Function 
     public boolean isEmpty() {
-        return list.size() == 0;
+        return head == null;
     }
 
     public void push(int data) {
-        list.add(data);
+        Node newNode = new Node(data);
+        if (isEmpty()) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
     }
 
     public void pop() {
-        list.remove(list.size() - 1);
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return;
+        }
+        head = head.next;
     }
 
     public int peek() {
-        return list.get(list.size() - 1);
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return head.data;
     }
 
+    // /// Stack Using Array List
+    // ArrayList<Integer> list = new ArrayList<>();
+    // // Is Empty Function 
+    // public boolean isEmpty() {
+    //     return list.size() == 0;
+    // }
+    // public void push(int data) {
+    //     list.add(data);
+    // }
+    // public void pop() {
+    //     list.remove(list.size() - 1);
+    // }
+    // public int peek() {
+    //     return list.get(list.size() - 1);
+    // }
+    public class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 }
