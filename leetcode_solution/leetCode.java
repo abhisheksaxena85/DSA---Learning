990182d9472d4ad5da89abcf91de1e5432d833b2
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-06-10 11:04:06 +0530

    Leetcode question 66. Plus One

--- leetcode_solution/leetCode.java
+++ leetcode_solution/leetCode.java
@@ -26,6 +26,30 @@ public class leetCode {
         // System.out.println(trap(arr));
     }
 
+    public static int[] plusOne(int[] digits) {
+        digits[digits.length - 1] = digits[digits.length - 1] + 1;
+
+        for (int i = digits.length - 1; i > 0; i--) {
+            if (digits[i] == 10) {
+                digits[i] = 0;
+                digits[i - 1] = digits[i - 1] + 1;
+            }
+        }
+
+        if (digits[0] == 10) {
+            int arr[] = new int[digits.length + 1];
+            arr[0] = 1;
+            arr[1] = 0;
+            for (int i = 2; i < digits.length; i++) {
+                arr[i] = digits[i];
+            }
+            return arr;
+        } else {
+            return digits;
+        }
+
+    }
+
     public static int[][] generateMatrix(int n) {
         int matrix[][] = new int[n][n];
         int counter = 1;
