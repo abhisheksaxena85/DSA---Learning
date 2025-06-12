ac6bf0e7a2caa98d27aaf125b2c8f809237dd7b9
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-06-12 13:31:03 +0530

    Starting the Queue Data Structure

---
+++ Chapter15/chapter_15.java
@@ -0,0 +1,11 @@
+/*
+    Starting Queue
+    Start Date - June 11, 2025, 3:51 PM
+ */
+
+public class chapter_15 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
