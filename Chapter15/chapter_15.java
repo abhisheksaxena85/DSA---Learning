2a1c04feac609b52ff2dc66ddd2934223cc40cc6
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-06-14 16:37:05 +0530

    Implementing Queue using the linkedlist

--- Chapter15/chapter_15.java
+++ Chapter15/chapter_15.java
@@ -7,7 +7,7 @@ public class chapter_15 {
 
     @SuppressWarnings("static-access")
     public static void main(String[] args) {
-        Queue queue = new Queue(4);
+        Queue queue = new Queue();
         queue.add(1);
         queue.add(2);
         queue.add(3);
@@ -24,65 +24,108 @@ public class chapter_15 {
 
     }
 
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
     static class Queue {
 
-        static int size; // size of queue
-        int arr[];
-        static int rear;
-        static int front;
+        static Node front = null;
+        static Node rear = null;
 
-        public Queue(int n) {
-            arr = new int[n];
-            size = n;
-            rear = -1;
-            front = -1;
-        }
-
-        // Check if queue is empty
         public boolean isEmpty() {
-            return rear == -1 && front == -1;
+            return front == null && rear == null;
         }
 
-        // Add new element in rear of queue
         public void add(int data) {
-            if (isFull()) {
-                System.out.println("Queue is filled!");
+            Node newNode = new Node(data);
+            if (front == null) {
+                front = rear = newNode;
                 return;
             }
-            if (front == -1) {
-                front = 0;
-            }
-            rear = (rear + 1) % size;
-            arr[rear] = data;
+            rear.next = newNode;
+            rear = newNode;
         }
 
-        // Check if queue is full 
-        public boolean isFull() {
-            return (rear + 1) % size == front;
-        }
-
-        // Remove the front element from queue
         public int remove() {
             if (isEmpty()) {
-                System.out.println("Queue is empty");
                 return -1;
             }
-            int frontVal = arr[front];
-            if (front == rear) {
-                front = rear = -1;
+
+            int val = front.data;
+            if (rear == front) {
+                rear = front = null;
             } else {
-                front = (front + 1) % size;
+                front = front.next;
             }
-            return frontVal;
+            return val;
         }
 
-        // Accessing first element of queue - as peek operation
         public int peek() {
             if (isEmpty()) {
-                System.out.println("Queue is empty");
                 return -1;
             }
-            return arr[front];
+            return front.data;
         }
+
+        // static int size; // size of queue
+        // int arr[];
+        // static int rear;
+        // static int front;
+        // public Queue(int n) {
+        //     arr = new int[n];
+        //     size = n;
+        //     rear = -1;
+        //     front = -1;
+        // }
+        // // Check if queue is empty
+        // public boolean isEmpty() {
+        //     return rear == -1 && front == -1;
+        // }
+        // // Add new element in rear of queue
+        // public void add(int data) {
+        //     if (isFull()) {
+        //         System.out.println("Queue is filled!");
+        //         return;
+        //     }
+        //     if (front == -1) {
+        //         front = 0;
+        //     }
+        //     rear = (rear + 1) % size;
+        //     arr[rear] = data;
+        // }
+        // // Check if queue is full 
+        // public boolean isFull() {
+        //     return (rear + 1) % size == front;
+        // }
+        // // Remove the front element from queue
+        // public int remove() {
+        //     if (isEmpty()) {
+        //         System.out.println("Queue is empty");
+        //         return -1;
+        //     }
+        //     int frontVal = arr[front];
+        //     if (front == rear) {
+        //         front = rear = -1;
+        //     } else {
+        //         front = (front + 1) % size;
+        //     }
+        //     return frontVal;
+        // }
+        // // Accessing first element of queue - as peek operation
+        // public int peek() {
+        //     if (isEmpty()) {
+        //         System.out.println("Queue is empty");
+        //         return -1;
+        //     }
+        //     return arr[front];
+        // }
     }
 }
