9cf5436323c879165c500b37687857a76cdc2ef7
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-06-18 16:36:34 +0530

    Activity Selection Problem with Greedy Algorithm

--- Chapter16/chapter_16.java
+++ Chapter16/chapter_16.java
@@ -6,6 +6,21 @@
 public class chapter_16 {
 
     public static void main(String[] ar) {
-        System.out.println("hello world");
+        int startTime[] = {1, 3, 0, 5, 8, 5};
+        int endTime[] = {2, 4, 6, 7, 9, 9};
+        System.out.println(activities(startTime, endTime));
+    }
+
+    public static int activities(int[] startTime, int[] endTime) {
+        int maxAct = 1;
+        int last = endTime[0];
+        for (int i = 1; i < endTime.length; i++) {
+            if (startTime[i] >= last) {
+                maxAct++;
+                last = endTime[i];
+            }
+        }
+
+        return maxAct;
     }
 }
