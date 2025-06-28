b40c3bdfa6b00e7e4116c81e87ded1d28a9698be
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-06-29 04:59:08 +0530

    Tree traversal - Preorder

--- Chapter17/chapter_17.java
+++ Chapter17/chapter_17.java
@@ -7,20 +7,30 @@ class chapter_17 {
 
     public static void main(String[] args) {
         int arr[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
-        Node result = buildTree(arr, -1);
-        System.out.println(result.left.left.data);
-        System.out.println(result.data);
+        Node result = buildTree(arr);
+        preorder(result);
     }
 
-    public static Node buildTree(int arr[], int index) {
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.data);
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    static int index = -1;
+
+    public static Node buildTree(int arr[]) {
         index++;
         if (arr[index] == -1) {
             return null;
         }
 
         Node newNode = new Node(arr[index]);
-        newNode.left = buildTree(arr, index);
-        newNode.right = buildTree(arr, index);
+        newNode.left = buildTree(arr);
+        newNode.right = buildTree(arr);
         return newNode;
     }
 
