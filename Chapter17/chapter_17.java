26847f4a406a09d0c11a64745551b4a2b3144291
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-06-29 05:16:03 +0530

    Tree traversal - Inorder

--- Chapter17/chapter_17.java
+++ Chapter17/chapter_17.java
@@ -8,7 +8,17 @@ class chapter_17 {
     public static void main(String[] args) {
         int arr[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         Node result = buildTree(arr);
-        preorder(result);
+        inorder(result);
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.println(root.data);
+        inorder(root.right);
     }
 
     public static void preorder(Node root) {
