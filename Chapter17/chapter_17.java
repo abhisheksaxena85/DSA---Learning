02928eb11a6f5fe8c0ab4baa6af6f260f15ccab3
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-06-29 04:22:06 +0530

    Preorder Sequence of Tree datastructure

--- Chapter17/chapter_17.java
+++ Chapter17/chapter_17.java
@@ -6,6 +6,34 @@
 class chapter_17 {
 
     public static void main(String[] args) {
-        System.out.println("Binary Trees");
+        int arr[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        Node result = buildTree(arr, -1);
+        System.out.println(result.left.left.data);
+        System.out.println(result.data);
+    }
+
+    public static Node buildTree(int arr[], int index) {
+        index++;
+        if (arr[index] == -1) {
+            return null;
+        }
+
+        Node newNode = new Node(arr[index]);
+        newNode.left = buildTree(arr, index);
+        newNode.right = buildTree(arr, index);
+        return newNode;
+    }
+
+    static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        Node(int val) {
+            data = val;
+            left = null;
+            right = null;
+        }
     }
 }
