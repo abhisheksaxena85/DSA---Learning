2dea00308eb2969fceea0cbef82b037816468acc
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-06-29 05:29:55 +0530

    Tree traversal - Postorder

--- Chapter17/chapter_17.java
+++ Chapter17/chapter_17.java
@@ -8,7 +8,17 @@ class chapter_17 {
     public static void main(String[] args) {
         int arr[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         Node result = buildTree(arr);
-        inorder(result);
+        postorder(result);
+    }
+
+    public static void postorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        postorder(root.left);
+        postorder(root.right);
+        System.out.println(root.data);
     }
 
     public static void inorder(Node root) {
