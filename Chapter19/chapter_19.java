7148840fcedbec4b5622916cd4fb465916ff9dcb
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-08-15 17:46:03 +0530

    Starting of priority queues

---
+++ Chapter19/chapter_19.java
@@ -0,0 +1,10 @@
+/*
+    Starting Heaps/Priority Queues
+    Start Date - Aug 15 2025, 05:45 PM
+ */
+
+class chapter_19 {
+    public static void main(String arg[]) {
+        System.out.println("Hello world");
+    }
+}
