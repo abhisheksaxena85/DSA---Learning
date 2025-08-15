574c406ac616500d26b170e9c688ee7b804d6d6b
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-08-15 22:54:28 +0530

    Java Collection Framework's Priority Queues implementation

--- Chapter19/chapter_19.java
+++ Chapter19/chapter_19.java
@@ -1,10 +1,17 @@
+
 /*
     Starting Heaps/Priority Queues
     Start Date - Aug 15 2025, 05:45 PM
  */
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 class chapter_19 {
     public static void main(String arg[]) {
-        System.out.println("Hello world");
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.add(1);
+        pq.add(4);
+        pq.add(11);
+        System.out.println(pq.peek());
     }
 }
