894d9cea83d96b9b6c06489f5a029c2d7c4ecec5
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-08-16 09:16:22 +0530

    Add function of priority queue implemented with arraylist in the heap with time-complexity of O(logn)

--- Chapter19/chapter_19.java
+++ Chapter19/chapter_19.java
@@ -3,16 +3,48 @@
     Starting Heaps/Priority Queues
     Start Date - Aug 15 2025, 05:45 PM
  */
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 class chapter_19 {
     public static void main(String arg[]) {
-        PriorityQueue<Student> pq = new PriorityQueue<>();
-        pq.add(new Student("A", 12));
-        pq.add(new Student("C", 1));
-        pq.add(new Student("D", 2));
-        System.out.println(pq.peek().name + " -- " + pq.peek().rank);
+        // PriorityQueue<Student> pq = new PriorityQueue<>();
+        // pq.add(new Student("A", 12));
+        // pq.add(new Student("C", 1));
+        // pq.add(new Student("D", 2));
+        // System.out.println(pq.peek().name + " -- " + pq.peek().rank);
+
+        Heap h = new Heap();
+        h.add(4);
+        h.add(2);
+        h.add(5);
+        h.add(1);
+        h.add(0);
+
+        System.out.println(h.peek());
+    }
+
+    static class Heap {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        public void add(int data) {
+            arr.add(data);
+            int x = arr.size() - 1; // x is child index
+            int par = (x - 1) / 2; // par index
+            while (arr.get(x) < arr.get(par)) {
+                // 0(logn) swap
+                int temp = arr.get(x);
+                arr.set(x, arr.get(par));
+                arr.set(par, temp);
+
+                x = par;
+                par = (x - 1) / 2;
+            }
+        }
+
+        public int peek() {
+            return arr.get(0);
+        }
     }
 
     static class Student implements Comparable<Student> {
