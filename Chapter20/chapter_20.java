663fbeb9947797531aa8c3851852b774766448ec
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-08-24 04:06:52 +0530

    Implementation of Hashmap

--- Chapter20/chapter_20.java
+++ Chapter20/chapter_20.java
@@ -1,10 +1,18 @@
 /*
-    Starting Hashing
+    Starting Hashing[Map(Hashmap, LinkedHasmap, Treemap), Sets(HashSet, LinkedSet, TreeSet)]
     Start Date - Aug 22 2025, 09:35 PM
  */
 
+import java.util.*;
+
 public class chapter_20 {
     public static void main(String[] args) {
-        System.out.println("Starting of Hashing leqture..");
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("India", 120);
+        map.put("China", 140);
+
+        map.remove("India");
+
+        System.out.println(map.get("India"));
     }
 }
