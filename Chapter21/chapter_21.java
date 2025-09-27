40e81991b27817c1a23dd050f0b058f47509dd76
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-09-28 00:31:21 +0530

    Insert and search operation on the Tries Data structure

--- Chapter21/chapter_21.java
+++ Chapter21/chapter_21.java
@@ -5,6 +5,51 @@
 
 class chapter_21 {
     public static void main(String args[]) {
-        System.out.println("");
+
+        String arr[] = { "the", "a", "there", "their", "any", "thee" };
+
+        for (int i = 0; i < arr.length; i++) {
+            insert(arr[i]);
+        }
+
+        System.out.println(search("asdf"));
+
+    }
+
+    public static boolean search(String strValue) {
+        Node curr = root;
+        for (int i = 0; i < strValue.length(); i++) {
+            int idx = strValue.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.endOfWord == true;
+    }
+
+    public static void insert(String str) {
+        Node curr = root;
+        for (int j = 0; j < str.length(); j++) {
+            int idx = str.charAt(j) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.endOfWord = true;
+    }
+
+    static Node root = new Node();
+
+    static class Node {
+        Node children[] = new Node[26];
+        boolean endOfWord = false;
+
+        Node() {
+            for (int i = 0; i < children.length; i++) {
+                children[i] = null;
+            }
+        }
     }
 }
