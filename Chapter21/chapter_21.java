b0c6967deb1488c2ecc7c7dfa23ce41ec52b863a
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-09-28 04:37:32 +0530

    Finding Shortest Unique Prefix - Hard Level Question

--- Chapter21/chapter_21.java
+++ Chapter21/chapter_21.java
@@ -16,41 +16,66 @@ class chapter_21 {
 
         // System.out.println(search("asdf"));
 
-        String arr[] = { "i", "like", "sam",
-                "samsung",
-                "mobile", "ice" };
-        String key = "ilikesamsung";
+        // String arr[] = { "i", "like", "sam",
+        // "samsung",
+        // "mobile", "ice" };
+        // String key = "ilikesamsung";
+
+        // for (int i = 0; i < arr.length; i++) {
+        // insert(arr[i]);
+        // }
+        // System.out.println(wordBreak(key));
+
+        String arr[] = { "zebra", "dog", "duck", "dove" };
 
         for (int i = 0; i < arr.length; i++) {
             insert(arr[i]);
         }
-        System.out.println(wordBreak(key));
+        root.freq = -1;
+
+        findPref(root, "");
 
     }
 
-    public static boolean wordBreak(String str) {
-        if (str.length() == 0) {
-            return true;
+    public static void findPref(Node node, String ans) {
+        if (node == null) {
+            return;
         }
-        for (int i = 1; i <= str.length(); i++) {
-            if (search(str.substring(0, i)) == true && wordBreak(str.substring(i)) == true) {
-                return true;
+        if (node.freq == 1) {
+            System.out.println(ans);
+            return;
+        }
+        for (int i = 0; i < node.children.length; i++) {
+            if (node.children[i] != null) {
+                findPref(node.children[i], ans + (char) (i + 'a'));
             }
         }
-        return false;
     }
 
-    public static boolean search(String strValue) {
-        Node curr = root;
-        for (int i = 0; i < strValue.length(); i++) {
-            int idx = strValue.charAt(i) - 'a';
-            if (curr.children[idx] == null) {
-                return false;
-            }
-            curr = curr.children[idx];
-        }
-        return curr.endOfWord == true;
-    }
+    // public static boolean wordBreak(String str) {
+    // if (str.length() == 0) {
+    // return true;
+    // }
+    // for (int i = 1; i <= str.length(); i++) {
+    // if (search(str.substring(0, i)) == true && wordBreak(str.substring(i)) ==
+    // true) {
+    // return true;
+    // }
+    // }
+    // return false;
+    // }
+
+    // public static boolean search(String strValue) {
+    // Node curr = root;
+    // for (int i = 0; i < strValue.length(); i++) {
+    // int idx = strValue.charAt(i) - 'a';
+    // if (curr.children[idx] == null) {
+    // return false;
+    // }
+    // curr = curr.children[idx];
+    // }
+    // return curr.endOfWord == true;
+    // }
 
     public static void insert(String str) {
         Node curr = root;
@@ -58,6 +83,8 @@ class chapter_21 {
             int idx = str.charAt(j) - 'a';
             if (curr.children[idx] == null) {
                 curr.children[idx] = new Node();
+            } else {
+                curr.children[idx].freq++;
             }
             curr = curr.children[idx];
         }
@@ -69,11 +96,13 @@ class chapter_21 {
     static class Node {
         Node children[] = new Node[26];
         boolean endOfWord = false;
+        int freq;
 
         Node() {
             for (int i = 0; i < children.length; i++) {
                 children[i] = null;
             }
+            freq = 1;
         }
     }
 }
