a29364a2fc772a9a2c8b377bdc4e7a566ea9415b
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-09-28 22:07:26 +0530

    Starting of Graph Data structure

---
+++ Chapter22/chapter_22.java
@@ -0,0 +1,10 @@
+/*
+ * Starting Graphs
+ * Start Date - Sep 28 2025, 10:03 PM
+ */
+
+class chapter_22 {
+    public static void main(String a[]) {
+        System.out.println("Starting of Graph");
+    }
+}
