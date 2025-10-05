ad8db61a99d7b6e3ac5ebea8ae6e0732b242e822
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-10-06 00:10:32 +0530

    Finding the cycle in Directed Graph

--- Chapter22/chapter_22.java
+++ Chapter22/chapter_22.java
@@ -57,64 +57,26 @@ class chapter_22 {
         // Add edges for an undirected graph (example)
         graph[0].add(new Edge(0, 1, 1));
         graph[0].add(new Edge(0, 2, 1));
+        graph[1].add(new Edge(2, 3, 1));
+        graph[2].add(new Edge(1, 3, 1));
 
-        graph[1].add(new Edge(1, 0, 1));
-        graph[1].add(new Edge(1, 3, 1));
+        // int col[] = new int[graph.length];
 
-        graph[2].add(new Edge(2, 0, 1));
-        graph[2].add(new Edge(2, 4, 1));
+        // for (int i = 0; i < col.length; i++) {
+        // col[i] = -1;
+        // }
 
-        graph[3].add(new Edge(3, 1, 1));
-        graph[3].add(new Edge(3, 4, 1));
-
-        graph[4].add(new Edge(4, 2, 1));
-        graph[4].add(new Edge(4, 3, 1));
-
-        int col[] = new int[graph.length];
-
-        for (int i = 0; i < col.length; i++) {
-            col[i] = -1;
-        }
-
-        System.out.println(biPart(graph, col));
+        // System.out.println(biPart(graph, col));
+        System.out.println(hasCycleDirected(graph));
     }
 
-    public static boolean biPart(ArrayList<Edge> graph[], int col[]) {
+    public static boolean hasCycleDirected(ArrayList<Edge> graph[]) {
+        boolean vis[] = new boolean[graph.length];
+        boolean stack[] = new boolean[graph.length];
+
         for (int i = 0; i < graph.length; i++) {
-            if (col[i] == -1) {
-                if (!isBiPartite(graph, col, i)) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static boolean isBiPartite(ArrayList<Edge> graph[], int col[], int vertex) {
-        Queue<Integer> q = new LinkedList<>();
-        col[vertex] = 0;
-        q.add(vertex);
-        while (!q.isEmpty()) {
-            int val = q.remove();
-
-            for (int i = 0; i < graph[val].size(); i++) {
-                Edge e = graph[val].get(i);
-                if (col[e.destination] == -1) {
-                    col[e.destination] = col[val] == 0 ? 1 : 0;
-                    q.add(e.destination);
-                } else if (col[e.destination] == col[val]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static boolean hasCycle(ArrayList<Edge>[] adjList) {
-        boolean visited[] = new boolean[adjList.length];
-        for (int i = 0; i < adjList.length; i++) {
-            if (!visited[i]) {
-                if (hashCycleUtil(adjList, i, -1, visited)) {
+            if (!vis[i]) {
+                if (hasCycleDirectedUtil(graph, i, vis, stack)) {
                     return true;
                 }
             }
@@ -122,54 +84,117 @@ class chapter_22 {
         return false;
     }
 
-    public static boolean hasPath(ArrayList<Edge> arr[], int src, int dst, boolean visited[]) {
-        if (src == dst)
-            return true;
+    public static boolean hasCycleDirectedUtil(ArrayList<Edge> graph[], int curr, boolean vis[], boolean stack[]) {
+        vis[curr] = true;
+        stack[curr] = true;
 
-        visited[src] = true;
-        for (int i = 0; i < arr[src].size(); i++) {
-            Edge e = arr[src].get(i);
-            if (!visited[e.destination] && hasPath(arr, e.destination, dst, visited)) {
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (stack[e.destination]) {
+                return true;
+            }
+            if (!stack[e.destination] && hasCycleDirectedUtil(graph, e.destination, vis, stack)) {
                 return true;
             }
         }
-        return false;
-
-    }
-
-    /// Depth First Search - Graph Traversal
-    public static boolean hashCycleUtil(ArrayList<Edge>[] arr, int curr, int par, boolean visited[]) {
-        visited[curr] = true;
-
-        for (int i = 0; i < arr[curr].size(); i++) {
-            Edge e = arr[curr].get(i);
-
-            if (visited[e.destination] && curr != par) {
-                return true;
-            } else if (!visited[e.destination]) {
-                if (hashCycleUtil(arr, e.destination, curr, visited)) {
-                    return true;
-                }
-            }
-        }
+        stack[curr] = false;
         return false;
     }
 
-    public static void bfs(ArrayList<Edge>[] adjList, boolean[] visited, int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
+    // public static boolean biPart(ArrayList<Edge> graph[], int col[]) {
+    // for (int i = 0; i < graph.length; i++) {
+    // if (col[i] == -1) {
+    // if (!isBiPartite(graph, col, i)) {
+    // return false;
+    // }
+    // }
+    // }
+    // return true;
+    // }
 
-        while (!q.isEmpty()) {
-            int val = q.remove();
-            if (visited[val] == false) {
-                System.out.println(val);
-                for (int i = 0; i < adjList[val].size(); i++) {
-                    q.add(adjList[val].get(i).destination);
-                }
-                visited[val] = true;
-            }
-        }
-    }
+    // public static boolean isBiPartite(ArrayList<Edge> graph[], int col[], int
+    // vertex) {
+    // Queue<Integer> q = new LinkedList<>();
+    // col[vertex] = 0;
+    // q.add(vertex);
+    // while (!q.isEmpty()) {
+    // int val = q.remove();
+    // for (int i = 0; i < graph[val].size(); i++) {
+    // Edge e = graph[val].get(i);
+    // if (col[e.destination] == -1) {
+    // col[e.destination] = col[val] == 0 ? 1 : 0;
+    // q.add(e.destination);
+    // } else if (col[e.destination] == col[val]) {
+    // return false;
+    // }
+    // }
+    // }
+    // return true;
+    // }
+
+    // public static boolean hasCycle(ArrayList<Edge>[] adjList) {
+    // boolean visited[] = new boolean[adjList.length];
+    // for (int i = 0; i < adjList.length; i++) {
+    // if (!visited[i]) {
+    // if (hashCycleUtil(adjList, i, -1, visited)) {
+    // return true;
+    // }
+    // }
+    // }
+    // return false;
+    // }
+
+    // public static boolean hasPath(ArrayList<Edge> arr[], int src, int dst,
+    // boolean visited[]) {
+    // if (src == dst)
+    // return true;
+
+    // visited[src] = true;
+    // for (int i = 0; i < arr[src].size(); i++) {
+    // Edge e = arr[src].get(i);
+    // if (!visited[e.destination] && hasPath(arr, e.destination, dst, visited)) {
+    // return true;
+    // }
+    // }
+    // return false;
+
+    // }
+
+    // /// Depth First Search - Graph Traversal
+    // public static boolean hashCycleUtil(ArrayList<Edge>[] arr, int curr, int par,
+    // boolean visited[]) {
+    // visited[curr] = true;
+
+    // for (int i = 0; i < arr[curr].size(); i++) {
+    // Edge e = arr[curr].get(i);
+
+    // if (visited[e.destination] && curr != par) {
+    // return true;
+    // } else if (!visited[e.destination]) {
+    // if (hashCycleUtil(arr, e.destination, curr, visited)) {
+    // return true;
+    // }
+    // }
+    // }
+    // return false;
+    // }
+
+    // public static void bfs(ArrayList<Edge>[] adjList, boolean[] visited, int
+    // start) {
+    // Queue<Integer> q = new LinkedList<>();
+    // q.add(start);
+
+    // while (!q.isEmpty()) {
+    // int val = q.remove();
+    // if (visited[val] == false) {
+    // System.out.println(val);
+    // for (int i = 0; i < adjList[val].size(); i++) {
+    // q.add(adjList[val].get(i).destination);
+    // }
+    // visited[val] = true;
+    // }
+    // }
+    // }
 
     static class Edge {
         int source, destination, weight;
